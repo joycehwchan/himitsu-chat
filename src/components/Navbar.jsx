4af91613a55796f251780f0702a1ea8bde3da2cb
joycehwchan <joycehwchan@gmail.com>
2022-12-22 21:30:00 +0900

    display current user info

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
     <div className="navbar">
       <div className="user">
         <img
-          src="https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          src={currentUser.photoURL}
           alt="user"
           className="avatar avatar-sm"
         />
-        <span>Name</span>
+        <span>{currentUser.displayName}</span>
       </div>
     </div>
   );
