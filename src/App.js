2ff10d0acf55770343a09e0284df6e5e5fb630ef
joycehwchan <joycehwchan@gmail.com>
2022-12-27 12:20:47 +0900

    change basename

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   };
 
   return (
-    <BrowserRouter basename={window.location.pathname || ""}>
+    <BrowserRouter basename={window.location || ""}>
       <Routes>
         <Route path="/">
           <Route
